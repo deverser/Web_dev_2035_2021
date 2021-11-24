2edfef6b01c38fd407e1518f40f29b0fb85174bf
Alexander <alexmal74@gmail.com>
2021-11-24 08:57:35 +0500

    Выполнены задания 4.6, 4.7

---
+++ Javascript/Objects/4.6.js
@@ -0,0 +1,4 @@
+let city = {};
+city['name'] = 'ГородN';
+city['population'] = '10 млн';
+console.log(city);
---
+++ Javascript/Objects/4.7.js
@@ -0,0 +1,5 @@
+let city = {};
+city['name'] = 'ГородN';
+city['population'] = '10 млн';
+city['getName'] = function () { return this.name; };
+console.log(city);
