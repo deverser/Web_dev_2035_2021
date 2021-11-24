2dc4a4c0b17fe00e4dc9fec4c4bc835f2c4f129a
Alexander <alexmal74@gmail.com>
2021-11-24 09:46:32 +0500

    Выполнено задание 4.8

---
+++ Javascript/Objects/4.8.js
@@ -0,0 +1,14 @@
+let city = {};
+city['name'] = 'ГородN';
+city['population'] = '10 млн';
+city['getName'] = function () { return this.name; };
+city['printProperties'] = function () {
+    let tmp = '';
+    for (let item in this) {
+        if((typeof this[item]) !== 'function') {
+            tmp = tmp + item + '=' + this[item] + '/';
+        }
+    }
+    return tmp;
+};
+console.log(city['printProperties']());
