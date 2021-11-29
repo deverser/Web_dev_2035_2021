39bbdfcbd10951e1a07f6ce539fec8dabff07c72
Alexander <alexmal74@gmail.com>
2021-11-30 00:45:07 +0500

    Выполнено задание 5.7

---
+++ Javascript/Window_timers/5.7.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function getDateProperties(milliseconds) {
+    let date = new Date(milliseconds);
+    let result = {};
+
+    result.day = date.getDay();
+    result.month = date.getMonth()+1;
+    result.year = date.getFullYear();
+    result.hour = date.getHours();
+    result.minutes = date.getMinutes();
+    result.seconds = date.getSeconds();
+
+    return result;
+}
+
+console.log(getDateProperties(214355323434));
