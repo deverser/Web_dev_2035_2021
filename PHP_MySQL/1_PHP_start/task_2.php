ec6e728d679e03fb06a8d98c46b39c577eabcc2d
Alexander <alexmal74@gmail.com>
2022-02-27 03:24:38 +0500

    Выполнено задание 2.2

---
+++ PHP_MySQL/1_PHP_start/task_2.php
@@ -0,0 +1,11 @@
+<?php
+$a = 1;
+    if ($a == 1 || $a == 2) {
+        echo "Hello World";
+    } elseif ($a == 3) {
+        echo "Hi world";
+    } else {
+        echo "Goodbye world";
+    }
+    
+?>
